0a775a37ec076a77bd4fe627fd2408e621c574e8
Agus Riyanto <agoos28@gmail.com>
2024-03-04 12:33:01 +0700

    tes

--- android/src/main/java/com/reactnativehintrequestpicker/HintRequestPickerModule.kt
+++ android/src/main/java/com/reactnativehintrequestpicker/HintRequestPickerModule.kt
@@ -4,35 +4,33 @@ import android.app.Activity
 import android.app.Activity.RESULT_OK
 import android.content.Intent
 import android.util.Log
-import android.widget.Toast
-import com.facebook.react.bridge.*
 import com.facebook.react.modules.core.DeviceEventManagerModule.RCTDeviceEventEmitter
-import com.google.android.gms.auth.api.credentials.Credential
+import com.google.android.gms.auth.api.identity.Identity
 import javax.annotation.Nullable
 
 
 class HintRequestPickerModule(private val reactContext: ReactApplicationContext) : ReactContextBaseJavaModule(reactContext), ActivityEventListener {
-    val hintPicker : HintRequestPicker
-    init {
-      reactContext.addActivityEventListener(this);
-      hintPicker = HintRequestPicker(reactContext);
-    }
+  private val hintPicker : HintRequestPicker
+  init {
+    reactContext.addActivityEventListener(this);
+    hintPicker = HintRequestPicker(reactContext);
+  }
 
-    override fun getName(): String {
-        return "HintRequestPicker"
-    }
+  override fun getName(): String {
+    return "HintRequestPicker"
+  }
 
-    // Example method
-    // See https://reactnative.dev/docs/native-modules-android
-    @ReactMethod
-    fun getPhoneNumber() {
-      hintPicker.getPhoneNumber();
-    }
+  // Example method
+  // See https://reactnative.dev/docs/native-modules-android
+  @ReactMethod
+  fun getPhoneNumber() {
+    hintPicker.getPhoneNumber();
+  }
 
   @ReactMethod
-  fun getGoogleAccount() {
+  fun getGoogleAccount(clientId: String?) {
     Log.d("Hint Request Module", "Getting Email");
-    hintPicker.getGoogleAccount();
+    hintPicker.getGoogleAccount(clientId);
   }
 
   private fun sendEvent(eventName: String,
@@ -46,44 +44,47 @@ class HintRequestPickerModule(private val reactContext: ReactApplicationContext)
     if(data == null){
       return;
     }
-    if (requestCode === Constants.PHONE_PICKER_REQUEST) {
+    if (requestCode == Constants.PHONE_PICKER_REQUEST) {
       val map = Arguments.createMap()
-      if (resultCode === RESULT_OK) {
-        val credential: Credential? = data.getParcelableExtra(Credential.EXTRA_KEY)
-        if (credential == null) {
-          return;
+      if (resultCode == RESULT_OK) {
+        if(activity != null) {
+          val phoneNumber = Identity.getSignInClient(activity).getPhoneNumberFromIntent(data)
+          map.putString("phoneNumber", phoneNumber);
         }
-        val phoneNumber = credential.getId();
-        map.putString("phoneNumber", phoneNumber);
       } else {
         map.putString("phoneNumber", null);
       }
       sendEvent(Constants.PHONE_SELECTED_EVENT, map)
     }
-    else if (requestCode === Constants.EMAIL_PICKER_REQUEST) {
+    else if (requestCode == Constants.EMAIL_PICKER_REQUEST) {
       val map = Arguments.createMap()
-      if (resultCode === RESULT_OK) {
-        val credential: Credential? = data.getParcelableExtra(Credential.EXTRA_KEY)
-        if (credential == null) {
-          return;
-        }
-        map.putString("givenName", credential.givenName);
-        map.putString("name", credential.name);
-        map.putString("id", credential.id);
-        map.putString("email", credential.id);
-        map.putString("familyName", credential.familyName);
-        map.putString("profilePictureUri", credential.profilePictureUri.toString());
-        map.putString("accountType", credential.accountType);
-      }
-      else {
-        map.putString("email", null);
-        map.putString("id", null);
-      }
+//      if (resultCode == RESULT_OK) {
+//        val credential: Credential? = data.getParcelableExtra(Credential.EXTRA_KEY)
+//          if (credential !== null) {
+//              val token = credential.idTokens;
+//
+//              if (token.size > 0) {
+//                  map.putString("tokenId", token.first().idToken);
+//              }
+//
+//              map.putString("givenName", credential.givenName);
+//              map.putString("name", credential.name);
+//              map.putString("id", credential.id);
+//              map.putString("email", credential.id);
+//              map.putString("familyName", credential.familyName);
+//              map.putString("profilePictureUri", credential.profilePictureUri.toString());
+//              map.putString("accountType", credential.accountType);
+//          }
+//      }
+//      else {
+//        map.putString("email", null);
+//        map.putString("id", null);
+//      }
       sendEvent(Constants.EMAIL_SELECTED_EVENT, map)
     }
   }
 
-  override fun getConstants(): Map<String, Any>? {
+  override fun getConstants(): Map<String, Any> {
     val constants: MutableMap<String, Any> = HashMap()
     constants["PHONE_SELECTED_EVENT"] = Constants.PHONE_SELECTED_EVENT
     constants["EMAIL_SELECTED_EVENT"] = Constants.EMAIL_SELECTED_EVENT
